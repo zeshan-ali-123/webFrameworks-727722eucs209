495ef9f8a4d061556766686364ec601e9f132c8c
zeshan-ali-123 <151006816+zeshan-ali-123@users.noreply.github.com>
2024-04-12 20:45:13 +0530

    Delete Day-3 directory

--- Day-3/practice_at_home/practice_at_home_1/src/main/java/com/example/practice_at_home_1/Repositories/StudentRepo.java
+++
@@ -1,5 +0,0 @@
-package com.example.practice_at_home_1.Repositories;
-
-public class StudentRepo {
-
-}
--- Day-3/practice_at_home/practice_at_home_1/src/main/java/com/example/practice_at_home_1/Service/ApiService.java
+++
@@ -1,5 +0,0 @@
-package com.example.practice_at_home_1.Service;
-
-public class ApiService {
-
-}
