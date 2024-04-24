914ae4b15be611bd4e6fa9f4ac69be8b9534c49c
zeshan-ali-123 <151006816+zeshan-ali-123@users.noreply.github.com>
2024-04-24 11:31:33 +0530

    Delete Day-6 directory

--- Day-6/challenge_yourself/day6cy/src/main/java/com/example/day6cy/repository/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day6cy.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day6cy.model.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Integer>{
-    
-}
--- Day-6/class_exercise/class_exercise_1/src/main/java/com/example/class_exercise_1/repository/ChildrenRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.class_exercise_1.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.class_exercise_1.model.Children;
-
-public interface ChildrenRepo extends JpaRepository<Children, Integer> {
-
-}
